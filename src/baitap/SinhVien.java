1b178501041c51a9ca03e6faa2e66e9dfc246a51
toanvipboy12345 <tminh3301747@gmail.com>
2024-12-13 20:26:57 +0700

    Bài 2: Sinh viên

---
+++ src/baitap/SinhVien.java
@@ -0,0 +1,49 @@
+package baitap;
+
+public class SinhVien {
+    private int rollNo;
+    private String name;
+    private String address;
+    private float mark;
+    public SinhVien() {}
+    public SinhVien(int rollNo, String name, String address) {
+        this.rollNo = rollNo;
+        this.name = name;
+        this.address = address;
+    }
+    public SinhVien(int rollNo, String name, String address, float mark) {
+        this.rollNo = rollNo;
+        this.name = name;
+        this.address = address;
+        this.mark = mark;
+    }
+    public int getRollNo() {
+        return rollNo;
+    }
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public float getMark() {
+        return mark;
+    }
+    public void setMark(float mark) {
+        this.mark = mark;
+    }
+
+
+
+}
